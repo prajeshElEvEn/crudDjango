4561273d7208537b86569aacf8f2ba5f35d98da1
Prajesh Pratap Singh <prajesh.eleven118@gmail.com>
2024-02-15 21:42:04 +0530

    added views

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
+    try:
+        question = get_object_or_404(Question, pk=question_id)
+        context = {"question": question}
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", context)
+
+
+def results(request, question_id):
+    context = {"response": "You're looking at the results of question",
+               "question_id": question_id}
+    return render(request, "polls/results.html", context)
+
+
+def vote(request, question_id):
+    context = {"response": "You're voting on question",
+               "question_id": question_id}
+    return render(request, "polls/vote.html", context)
